c2c002818f5ced955b0ab05189608c8f16876bb9
lyle-smal <lylesmal@gmail.com>
2026-06-15 17:26:13 +0200

    initial commit

---
+++ 2D Open World/wolf.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="wolf" tilewidth="16" tileheight="16" tilecount="960" columns="40">
+ <image source="gfx/wolfsheet1.png" width="640" height="384"/>
+</tileset>
